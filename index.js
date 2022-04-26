e678c67f75f794ffb13154a7b13104ed379a2448
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-04-26 16:25:06 +0100

    Created command-line help menu

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const program = require('commander')
+const log = console.log
+
+program.version('1.0.0').description('Simple Password Generator')
+
+program
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save password to password.txt')
+    .option('-nn, --no-numbers', 'remove numbers')
+    .option('-ns, --no-symbols', 'remove symbols')
+    .parse()
+
+const { length, save, numbers, symbols } = program.opts()
+
+// Get generated password
+const generatedPassword = createPassword(length, numbers, symbols)
+
+// Output generated password
+log(generatedPassword)
